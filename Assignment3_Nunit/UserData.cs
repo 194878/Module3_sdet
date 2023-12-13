8dbf4e4ab2447b9422781bc082ef8471a8b71f7b
194878 <Shahanas.Pulickaljaleel@ust.com>
2023-12-13 14:29:15 +0530

    13-12-23

---
+++ Assignment3_Nunit/UserData.cs
@@ -0,0 +1,31 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment3_Nunit
+{
+    
+        public class UserData
+        {
+            [JsonProperty("id")]
+            public int Id { get; set; }
+
+            [JsonProperty("userId")]
+            public string? UserId { get; set; }
+
+            [JsonProperty("title")]
+            public string? Title { get; set; }
+
+            [JsonProperty("completed")]
+            public string? Completed { get; set; }
+        }
+        public class UserDataResponse
+        {
+
+            public UserData? Data { get; set; }
+        }
+    }
+
